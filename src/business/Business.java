e070c35857cf8870e9169b5867d9fe2462474ba2
dbgs2 <eliseix@gmail.com>
2017-11-06 06:45:40 +0000

    forgot to work on a feature branch....

--- src/business/Business.java
+++ src/business/Business.java
@@ -1,14 +1,32 @@
 package business;
 
+import java.util.Arrays;
+
 public class Business {
 
-	public static int sum(int a, int b) {
-		return a + b;
-	}
+	public static char[] board;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	// New board set ' ' = empty square on board.
+	public static char[] setBoard() {
+		board = new char[] {' ',' ',' ',' ',' ',' ',' ',' ',' '};		
+		return  board;	
+	}
+
+	// Print current board out
+	public static String printBoard() {
+		setBoard();
+		return Arrays.toString(board);
+	}
+
+	// Player makes a move to board
+	public static void playerMove(char player, int move) {	
+		if(move < 0 || move > 9) return;
+		if(board[move] == ' ') board[move] = player;
+		
+	}
 }
--- src/businessTest/BusinessTest.java
+++ src/businessTest/BusinessTest.java
@@ -1,13 +1,48 @@
 package businessTest;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
 import org.junit.jupiter.api.Test;
 import business.Business;
 
 public class BusinessTest {
+
 	@Test
-	public void testNewFunction() {
-		assertEquals(2, Business.sum(1, 1));
+	public void testSetBoard() {
+		
+		// Empty board tested
+		char[] emptyBoard;
+		emptyBoard = new char[] {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+		assertArrayEquals(emptyBoard, Business.setBoard());
+	}
+	
+	@Test
+	public void testPrintBoard() {
+		
+		// Print current board test
+		String emptyBoardPrinted;
+		emptyBoardPrinted = "[ ,  ,  ,  ,  ,  ,  ,  ,  ]";
+		assertEquals(emptyBoardPrinted , Business.printBoard());
+	}
+	
+	@Test
+	public void testPlayerMove() {
+		
+		// Player makes a move test
+		char[] emptyBoard;
+		emptyBoard = new char[] {'X',' ',' ',' ',' ',' ',' ',' ',' '};
+		Business.playerMove('X', 0);
+		Business.playerMove('O', 0);
+		Business.playerMove('O', -1);
+		Business.playerMove('O', 10);
+		assertArrayEquals(emptyBoard, Business.board);
+		
+		emptyBoard = new char[] {'X','O',' ',' ',' ',' ',' ',' ',' '};
+		Business.playerMove('X', 0);
+		Business.playerMove('O', 1);
+		assertArrayEquals(emptyBoard, Business.board);
+		
 	}
 
 }
